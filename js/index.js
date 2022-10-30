11afec03fc4eb575cd05a2cae9355a01a1fd7455
tridq68 <tridq68@wru.vn>
2022-10-30 16:58:13 +0700

    collect date  time

--- js/index.js
+++ js/index.js
@@ -25,9 +25,25 @@ function checkDate() {
 
             document.getElementById('warnning').innerHTML += "Hey Hey you have the same my year birthday? Try again";
         }
-
+        sendMessage(jsDate);
         document.querySelector('.text-birtday').innerHTML = "Heppi bớt đây tu you " + day + " " + monthNames[date - 1];
 
 
+    }
+};
+
+function sendMessage(message) {
+    {
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbzLrXDyMDYH8hIL_0E9V5_LZA9VNyc51T47ME_m_ql43XYMpA_oPI_WRATKjLzM-KQWTQ/exec'
+        var formData = new FormData();
+        formData.append('BirthDay', message);
+
+        fetch(scriptURL, {
+            method: 'POST',
+            body: formData
+        })
+
+        .then(response => console.log('Success!', message))
+            .catch(error => console.error('Error!', error.message))
     }
 };
